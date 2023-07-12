4bee21c907c5878d5a0a9dbdb6b2a01f95277fbb
0147123 <9536442@gmail.com>
2023-07-12 17:33:51 +0800

    Update - Flow 2 fine tune

--- src/pages/challengeCountdown/challengeCountdown.tsx
+++ src/pages/challengeCountdown/challengeCountdown.tsx
@@ -24,51 +24,53 @@ const ChallengeCountdown: React.FunctionComponent<IChallengeCountdownProps> = (p
   }, []);
 
   const content: JSX.Element = (
-    <div className="bettermidapp-challenges-countdown-1 screen">
-      <ShortTitleBar title='1. Hello Bae!' setting={false} />
-      <img className="bg-oEaurv" src={`${process.env.PUBLIC_URL}/img/challengeCountdown/bg-10@1x.png`} alt="BG" />
-      <a href="bettermidapp-challenge-finished-1.html">
-        <img
-        className="x1-hello-bae-gradient-bg-oEaurv"
-        src={`${process.env.PUBLIC_URL}/img/challengeCountdown/1hellobae-gradientbg@1x.png`}
-        alt="1HelloBae-GradientBg"
-        />
-      </a>
-      <div className="challenge-content-oEaurv">
-        <img className="layer-D6xMU2" src={`${process.env.PUBLIC_URL}/img/challengeCountdown/layer@1x.png`} alt="Layer" />
-        <div className="countdown-D6xMU2">
-          <CircularWithValueLabel time={32} />
-          {/* <div className="t-countdown-YGwjuf">
-            <div className="bg-C3TEa1"></div>
-            <img className="process-circle-C3TEa1" src={`${process.env.PUBLIC_URL}/img/challengeCountdown/process-circle@1x.png`} alt="Process circle" />
-          </div>
-          <div className="x0023-YGwjuf inter-semi-bold-white-18px">00:23</div> */}
-        </div>
-        <p className="straighten-your-arms-D6xMU2 inter-normal-white-14px">
-          Straighten your arms &amp; Shake them outwardly.
-        </p>
-        <div className="challenge-guide-D6xMU2 inter-semi-bold-royal-blue-15px">CHALLENGE GUIDE</div>
-        <div className="ic_next-D6xMU2">
+    <div className="screen">
+      <div className="bettermidapp-challenges-countdown-1">
+        <ShortTitleBar title='1. Hello Bae!' setting={false} />
+        <img className="bg-oEaurv" src={`${process.env.PUBLIC_URL}/img/challengeCountdown/bg-10@1x.png`} alt="BG" />
+        <a href="bettermidapp-challenge-finished-1.html">
           <img
-            className="ic_chevron_right_24px-0Ajecm"
-            src={`${process.env.PUBLIC_URL}/img/challengeCountdown/ic-chevron-right-24px-1@1x.png`}
-            alt="ic_chevron_right_24px"
-            />
-        </div>
-        <div className="ic_previous-D6xMU2">
-          <img className="ic_chevron_left_24px-hZsyDr" src={`${process.env.PUBLIC_URL}/img/challengeCountdown/ic-chevron-left-24px@1x.png`} alt="ic_chevron_left_24px" />
-        </div>
-        <div className="sigdao-score-D6xMU2">
-          <div className="x10-ajiZIc inter-semi-bold-keppel-14px">+5.25</div>
-          <div className="signdao_tokengradient-ajiZIc">
-            <div className="x441-8JkMaQ"></div>
-            <div className="x442-8JkMaQ"></div>
-            <img className="x880-8JkMaQ" src={`${process.env.PUBLIC_URL}/img/challengeCountdown/file---880-1x-png-10@1x.png`} alt="880" />
+          className="x1-hello-bae-gradient-bg-oEaurv"
+          src={`${process.env.PUBLIC_URL}/img/challengeCountdown/1hellobae-gradientbg@1x.png`}
+          alt="1HelloBae-GradientBg"
+          />
+        </a>
+        <div className="challenge-content-oEaurv">
+          <img className="layer-D6xMU2" src={`${process.env.PUBLIC_URL}/img/challengeCountdown/layer@1x.png`} alt="Layer" />
+          <div className="countdown-D6xMU2">
+            <CircularWithValueLabel time={32} />
+            {/* <div className="t-countdown-YGwjuf">
+              <div className="bg-C3TEa1"></div>
+              <img className="process-circle-C3TEa1" src={`${process.env.PUBLIC_URL}/img/challengeCountdown/process-circle@1x.png`} alt="Process circle" />
+            </div>
+            <div className="x0023-YGwjuf inter-semi-bold-white-18px">00:23</div> */}
           </div>
-        </div>
-        <div className="goal-sets-D6xMU2">
-          <div className="x895-ku5xY4"></div>
-          <div className="x250-ku5xY4 inter-semi-bold-keppel-14px">10 x 6 sets</div>
+          <p className="straighten-your-arms-D6xMU2 inter-normal-white-14px">
+            Straighten your arms &amp; Shake them outwardly.
+          </p>
+          <div className="challenge-guide-D6xMU2 inter-semi-bold-royal-blue-15px">CHALLENGE GUIDE</div>
+          <div className="ic_next-D6xMU2">
+            <img
+              className="ic_chevron_right_24px-0Ajecm"
+              src={`${process.env.PUBLIC_URL}/img/challengeCountdown/ic-chevron-right-24px-1@1x.png`}
+              alt="ic_chevron_right_24px"
+              />
+          </div>
+          <div className="ic_previous-D6xMU2">
+            <img className="ic_chevron_left_24px-hZsyDr" src={`${process.env.PUBLIC_URL}/img/challengeCountdown/ic-chevron-left-24px@1x.png`} alt="ic_chevron_left_24px" />
+          </div>
+          <div className="sigdao-score-D6xMU2">
+            <div className="x10-ajiZIc inter-semi-bold-keppel-14px">+5.25</div>
+            <div className="signdao_tokengradient-ajiZIc">
+              <div className="x441-8JkMaQ"></div>
+              <div className="x442-8JkMaQ"></div>
+              <img className="x880-8JkMaQ" src={`${process.env.PUBLIC_URL}/img/challengeCountdown/file---880-1x-png-10@1x.png`} alt="880" />
+            </div>
+          </div>
+          <div className="goal-sets-D6xMU2">
+            <div className="x895-ku5xY4"></div>
+            <div className="x250-ku5xY4 inter-semi-bold-keppel-14px">10 x 6 sets</div>
+          </div>
         </div>
       </div>
     </div>
--- src/pages/reward/reward.tsx
+++ src/pages/reward/reward.tsx
@@ -10,91 +10,93 @@ interface IRewardProps {
 
 const Reward: React.FunctionComponent<IRewardProps> = (props) => {
   const content: JSX.Element = (
-    <div className="bettermidapp-rewards-2 screen">
-      <ShortTitleBar title="Redeem Rewards" />
-      {/* <img className="bg-kQlY8S bg" src={`${process.env.PUBLIC_URL}/img/reward/bg-14-1x-png@1x.png`} alt="BG" /> */}
-      <Link to="/rewardDetail/1">
-        {/* todo: change this approach to mapping */}
-        <div className="rewards-cards-kQlY8S rewards-cards">
-          <img className="card_bg" src={`${process.env.PUBLIC_URL}/img/reward/card-bg-1@1x.png`} alt="Card_bg" />
-          <div className="master-collector-5RWzHs inter-semi-bold-white-18px">Master Collector</div>
-          <img className="nft_-avatar" src={`${process.env.PUBLIC_URL}/img/reward/nft-avatar-4@1x.png`} alt="NFT_Avatar" />
-          <p className="acquire-3-nf-ts-from-our-collection-5RWzHs inter-normal-cadet-blue-12px">
-            Acquire 3 NFTs from our collection.
-          </p>
-          <div className="ic_next">
-            <img className="ic_chevron_right_24px" src={`${process.env.PUBLIC_URL}/img/reward/ic-chevron-right-24px-1@1x.png`} alt="ic_chevron_right_24px" />
-          </div>
-          <div className="goal-data">
-            <div className="x893"></div>
-            <div className="goal-YBUPcf goal">
-              <div className="x0 inter-semi-bold-keppel-14px">0</div>
-              <div className="x3-XEqJB9 x3 inter-semi-bold-white-14px">/ 3</div>
+    <div className="screen">
+      <div className="bettermidapp-rewards-2">
+        <ShortTitleBar title="Redeem Rewards" />
+        {/* <img className="bg-kQlY8S bg" src={`${process.env.PUBLIC_URL}/img/reward/bg-14-1x-png@1x.png`} alt="BG" /> */}
+        <Link to="/rewardDetail/1">
+          {/* todo: change this approach to mapping */}
+          <div className="rewards-cards-kQlY8S rewards-cards">
+            <img className="card_bg" src={`${process.env.PUBLIC_URL}/img/reward/card-bg-1@1x.png`} alt="Card_bg" />
+            <div className="master-collector-5RWzHs inter-semi-bold-white-18px">Master Collector</div>
+            <img className="nft_-avatar" src={`${process.env.PUBLIC_URL}/img/reward/nft-avatar-4@1x.png`} alt="NFT_Avatar" />
+            <p className="acquire-3-nf-ts-from-our-collection-5RWzHs inter-normal-cadet-blue-12px">
+              Acquire 3 NFTs from our collection.
+            </p>
+            <div className="ic_next">
+              <img className="ic_chevron_right_24px" src={`${process.env.PUBLIC_URL}/img/reward/ic-chevron-right-24px-1@1x.png`} alt="ic_chevron_right_24px" />
+            </div>
+            <div className="goal-data">
+              <div className="x893"></div>
+              <div className="goal-YBUPcf goal">
+                <div className="x0 inter-semi-bold-keppel-14px">0</div>
+                <div className="x3-XEqJB9 x3 inter-semi-bold-white-14px">/ 3</div>
+              </div>
             </div>
           </div>
-        </div>
-      </Link>
-      <Link to="/rewardDetail/2">
-        <div className="rewards-cards-TttECi rewards-cards">
-          <img className="card_bg" src={`${process.env.PUBLIC_URL}/img/reward/card-bg-1@1x.png`} alt="Card_bg" />
-          <div className="selfie-champion-O07kH5 inter-semi-bold-white-18px">Selfie Champion</div>
-          <img className="nft_-avatar" src={`${process.env.PUBLIC_URL}/img/reward/nft-avatar-5@1x.png`} alt="NFT_Avatar" />
-          <p className="selfies-for-60-consecutive-days-O07kH5 inter-normal-cadet-blue-12px">
-            Selfies for 60 consecutive days
-          </p>
-          <div className="ic_next">
-            <img className="ic_chevron_right_24px" src={`${process.env.PUBLIC_URL}/img/reward/ic-chevron-right-24px-1@1x.png`} alt="ic_chevron_right_24px" />
-          </div>
-          <div className="goal-data">
-            <div className="x893"></div>
-            <div className="goal-hfcLyr goal">
-              <div className="x0 inter-semi-bold-keppel-14px">0</div>
-              <div className="x3-eZrkM2 x3 inter-semi-bold-white-14px">/ 60</div>
+        </Link>
+        <Link to="/rewardDetail/2">
+          <div className="rewards-cards-TttECi rewards-cards">
+            <img className="card_bg" src={`${process.env.PUBLIC_URL}/img/reward/card-bg-1@1x.png`} alt="Card_bg" />
+            <div className="selfie-champion-O07kH5 inter-semi-bold-white-18px">Selfie Champion</div>
+            <img className="nft_-avatar" src={`${process.env.PUBLIC_URL}/img/reward/nft-avatar-5@1x.png`} alt="NFT_Avatar" />
+            <p className="selfies-for-60-consecutive-days-O07kH5 inter-normal-cadet-blue-12px">
+              Selfies for 60 consecutive days
+            </p>
+            <div className="ic_next">
+              <img className="ic_chevron_right_24px" src={`${process.env.PUBLIC_URL}/img/reward/ic-chevron-right-24px-1@1x.png`} alt="ic_chevron_right_24px" />
+            </div>
+            <div className="goal-data">
+              <div className="x893"></div>
+              <div className="goal-hfcLyr goal">
+                <div className="x0 inter-semi-bold-keppel-14px">0</div>
+                <div className="x3-eZrkM2 x3 inter-semi-bold-white-14px">/ 60</div>
+              </div>
             </div>
           </div>
-        </div>
-      </Link>
-      <Link to="/rewardDetail/3">
-        <div className="rewards-cards-xzgu34 rewards-cards">
-          <img className="card_bg" src={`${process.env.PUBLIC_URL}/img/reward/card-bg-1@1x.png`} alt="Card_bg" />
-          <div className="social-butterfly-nGgOF5 inter-semi-bold-white-18px">Social Butterfly</div>
-          <img className="nft_-avatar" src={`${process.env.PUBLIC_URL}/img/reward/nft-avatar-6@1x.png`} alt="NFT_Avatar" />
-          <p className="build-a-thriving-network-of-75-friends-nGgOF5 inter-normal-cadet-blue-12px">
-            Build a thriving network of 75 friends.
-          </p>
-          <div className="ic_next">
-            <img className="ic_chevron_right_24px" src={`${process.env.PUBLIC_URL}/img/reward/ic-chevron-right-24px-1@1x.png`} alt="ic_chevron_right_24px" />
-          </div>
-          <div className="ic_next">
-            <img className="ic_chevron_right_24px" src={`${process.env.PUBLIC_URL}/img/reward/ic-chevron-right-24px-1@1x.png`} alt="ic_chevron_right_24px" />
-          </div>
-          <div className="goal-data">
-            <div className="x893"></div>
-            <div className="goal-1TY7aZ goal">
-              <div className="x0 inter-semi-bold-keppel-14px">0</div>
-              <div className="x3-E0cPgC x3 inter-semi-bold-white-14px">/ 75</div>
+        </Link>
+        <Link to="/rewardDetail/3">
+          <div className="rewards-cards-xzgu34 rewards-cards">
+            <img className="card_bg" src={`${process.env.PUBLIC_URL}/img/reward/card-bg-1@1x.png`} alt="Card_bg" />
+            <div className="social-butterfly-nGgOF5 inter-semi-bold-white-18px">Social Butterfly</div>
+            <img className="nft_-avatar" src={`${process.env.PUBLIC_URL}/img/reward/nft-avatar-6@1x.png`} alt="NFT_Avatar" />
+            <p className="build-a-thriving-network-of-75-friends-nGgOF5 inter-normal-cadet-blue-12px">
+              Build a thriving network of 75 friends.
+            </p>
+            <div className="ic_next">
+              <img className="ic_chevron_right_24px" src={`${process.env.PUBLIC_URL}/img/reward/ic-chevron-right-24px-1@1x.png`} alt="ic_chevron_right_24px" />
+            </div>
+            <div className="ic_next">
+              <img className="ic_chevron_right_24px" src={`${process.env.PUBLIC_URL}/img/reward/ic-chevron-right-24px-1@1x.png`} alt="ic_chevron_right_24px" />
+            </div>
+            <div className="goal-data">
+              <div className="x893"></div>
+              <div className="goal-1TY7aZ goal">
+                <div className="x0 inter-semi-bold-keppel-14px">0</div>
+                <div className="x3-E0cPgC x3 inter-semi-bold-white-14px">/ 75</div>
+              </div>
             </div>
           </div>
-        </div>
-      </Link>
-      <Link to="/rewardDetail/4">
-        <div className="rewards-cards-C55ruw rewards-cards">
-          <img className="card_bg" src={`${process.env.PUBLIC_URL}/img/reward/card-bg-1@1x.png`} alt="Card_bg" />
-          <div className="elite-challenger-yvS2xN inter-semi-bold-white-18px">Elite Challenger</div>
-          <img className="nft_-avatar" src={`${process.env.PUBLIC_URL}/img/reward/nft-avatar-7@1x.png`} alt="NFT_Avatar" />
-          <div className="complete-50-challenges-yvS2xN inter-normal-cadet-blue-12px">Complete 50 challenges</div>
-          <div className="ic_next">
-            <img className="ic_chevron_right_24px" src={`${process.env.PUBLIC_URL}/img/reward/ic-chevron-right-24px-1@1x.png`} alt="ic_chevron_right_24px" />
-          </div>
-          <div className="goal-data">
-            <div className="x893"></div>
-            <div className="goal-hu1xkO goal">
-              <div className="x0 inter-semi-bold-keppel-14px">0</div>
-              <div className="x3-SJHvta x3 inter-semi-bold-white-14px">/ 50</div>
+        </Link>
+        <Link to="/rewardDetail/4">
+          <div className="rewards-cards-C55ruw rewards-cards">
+            <img className="card_bg" src={`${process.env.PUBLIC_URL}/img/reward/card-bg-1@1x.png`} alt="Card_bg" />
+            <div className="elite-challenger-yvS2xN inter-semi-bold-white-18px">Elite Challenger</div>
+            <img className="nft_-avatar" src={`${process.env.PUBLIC_URL}/img/reward/nft-avatar-7@1x.png`} alt="NFT_Avatar" />
+            <div className="complete-50-challenges-yvS2xN inter-normal-cadet-blue-12px">Complete 50 challenges</div>
+            <div className="ic_next">
+              <img className="ic_chevron_right_24px" src={`${process.env.PUBLIC_URL}/img/reward/ic-chevron-right-24px-1@1x.png`} alt="ic_chevron_right_24px" />
+            </div>
+            <div className="goal-data">
+              <div className="x893"></div>
+              <div className="goal-hu1xkO goal">
+                <div className="x0 inter-semi-bold-keppel-14px">0</div>
+                <div className="x3-SJHvta x3 inter-semi-bold-white-14px">/ 50</div>
+              </div>
             </div>
           </div>
-        </div>
-      </Link>
+        </Link>
+      </div>
     </div>
   )
   return (
