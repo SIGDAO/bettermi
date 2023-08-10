4c789c25345574bad345e669f1c1bec81de4acee
cychiuak <cychiuak@connect.ust.hk>
2023-08-10 11:08:33 +0800

    updated description

--- src/pages/rewardDetail/rewardDetail.tsx
+++ src/pages/rewardDetail/rewardDetail.tsx
@@ -21,18 +21,14 @@ const rewardDetailList: rewardDetailListProps[] = [
     id: 1,
     title: 'Master Collector',
     reward: 20,
-    description: `Unlocked by users who acquire 3 NFTs from our collection. As a reward, users are granted 
-                  a significant boost of 500 energy and 50 gems, 
-                  empowering them with valuable resources to enhance their overall experience.`,
+    description: `Unlocked by users who acquire 3 NFTs from our collection. As a reward, users are granted 20 Sigdao, empowering them with valuable resources to enhance their overall experience.`,
     bgImagePath: 'photo-1@1x.png',
     requireTimes: 3,
   },{
     id: 2,
     title: 'Selfie Champion',
     reward: 'Get a Random NFT.',
-    description: `Earned by users who maintain a consistent practice of capturing their progress through selfies 
-                  for 60 consecutive days. This remarkable commitment is acknowledged with the unlocking of a rare and coveted NFT, 
-                  symbolizing their dedication and perseverance.`,
+    description: `Earned by users who maintain a consistent practice of capturing their progress through selfies for 60 consecutive days. This remarkable commitment is acknowledged with the unlocking of a rare and coveted NFT, symbolizing their dedication and perseverance.`,
     bgImagePath: 'photo-3@1x.png',
     // sigdao: 20,
     requireTimes: 60,
@@ -50,9 +46,7 @@ const rewardDetailList: rewardDetailListProps[] = [
     id: 4,
     title: 'Elite Challenger',
     reward: 'Receive our exclusive healthy product',
-    description: `Users who complete 50 challenges, whether accumulated over time or in one continuous effort, 
-                  unlock this achievement. As a result, they gain access to an enhanced AI coach service, providing valuable guidance 
-                  and support to overcome future obstacles.`,
+    description: `Users who complete 50 challenges, whether accumulated over time or in one continuous effort, unlock this achievement. As a result, users receive a complimentary healthy product to our exclusive partner lifestyle service, celebrating your progress and elevating your fitness journey even further. `,
     bgImagePath: 'photo-1-1x-png@1x.png',
     requireTimes: 50,
   }
